fba8c32440120ce5a20d8790b3bdadac6b524729
Pan Li <pan2.li@intel.com>
2022-09-18 22:32:44 +0800

    Add print function for extension validate result
    Signed-off-by: Pan Li <pan2.li@intel.com>

--- common/apis.h
+++ common/apis.h
@@ -4,12 +4,14 @@
 #include "defines.h"
 #include "types.h"
 
-extern s_validate_result_collection_t *
-create_result_collection(uint32 capacity);
-extern void
-dispose_result_collection(s_validate_result_collection_t **collection);
-extern void append_one_result(s_validate_result_collection_t *collection,
-                              s_validate_result_t result);
+extern s_validate_result_collection_t * create_result_collection(
+  uint32 capacity);
+extern void dispose_result_collection(
+  s_validate_result_collection_t **collection);
+extern void append_one_result(
+  s_validate_result_collection_t *collection, s_validate_result_t result);
+extern void print_result_collection(
+  s_validate_result_collection_t *collection);
 
 extern s_validate_result_collection_t *validate_extensions();
 
