1b22f7784084004697dbc686e14abc51d9fbe51c
Restyled.io <commits@restyled.io>
2022-09-22 07:45:29 +0000

    Restyled by astyle

--- functionalities/auto_vectorization.c
+++ functionalities/auto_vectorization.c
@@ -4,32 +4,32 @@
 #include "../common/types.h"
 
 static inline uint64 inner_loop_nn_vectorization() {
-  int loop_size = 4096;
-  uint64 *a = malloc(sizeof(uint64) * loop_size);
-  uint64 *b = malloc(sizeof(uint64) * loop_size);
-  uint64 *c = malloc(sizeof(uint64) * loop_size);
+    int loop_size = 4096;
+    uint64 *a = malloc(sizeof(uint64) * loop_size);
+    uint64 *b = malloc(sizeof(uint64) * loop_size);
+    uint64 *c = malloc(sizeof(uint64) * loop_size);
 
-  FENCE();
+    FENCE();
 
-  for (int i = 0; i < loop_size; i++) {
-    c[i] = a[i] & b[i];
-  }
+    for (int i = 0; i < loop_size; i++) {
+        c[i] = a[i] & b[i];
+    }
 
-  FENCE();
+    FENCE();
 
-  uint64 result = c[rand() % loop_size];
+    uint64 result = c[rand() % loop_size];
 
-  free(a);
-  free(b);
-  free(c);
+    free(a);
+    free(b);
+    free(c);
 
-  return result;
+    return result;
 }
 
 uint64 show_auto_vectorization() {
-  uint64 result = 0;
+    uint64 result = 0;
 
-  result += inner_loop_nn_vectorization();
+    result += inner_loop_nn_vectorization();
 
-  return result;
+    return result;
 }
